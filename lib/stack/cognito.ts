f2e715ae57983e62b27b9019fd850cb7264f4159
tmokmss <tomookam@live.jp>
2020-12-28 16:21:36 +0900

    initial commit

---
+++ lib/stack/cognito.ts
@@ -0,0 +1,34 @@
+import * as cdk from '@aws-cdk/core';
+import * as cognito from '@aws-cdk/aws-cognito';
+import { NetworkStack } from './network';
+
+export interface CognitoStackProps extends cdk.StackProps {
+    readonly network?: NetworkStack,
+}
+
+export class CognitoStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: CognitoStackProps) {
+        super(scope, id, props);
+        const userPool = new cognito.UserPool(this, 'userPool', {
+            passwordPolicy: {
+                requireUppercase: false,
+                requireSymbols: false,
+                requireDigits: false,
+            },
+            selfSignUpEnabled: true,
+            signInAliases: {
+                username: false,
+                email: true
+            },
+        });
+
+        userPool.addClient(`mobileClient`, {
+
+        })
+
+        // new cognito.CfnUserPoolGroup(this, 'admin-group', {
+        //     userPoolId: userPool.userPoolId,
+        //     groupName: 'admin',
+        // })
+    }
+}
---
+++ lib/stack/ticket-service.ts
@@ -0,0 +1,36 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as gw from '@aws-cdk/aws-apigateway';
+import * as lambdanode from '@aws-cdk/aws-lambda-nodejs';
+import { GenericWindowsImage } from '@aws-cdk/aws-ec2';
+
+export interface TicketServiceStackProps extends cdk.StackProps {
+}
+
+export class TicketServiceStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: TicketServiceStackProps) {
+        super(scope, id, props);
+
+        const handler = new lambdanode.NodejsFunction(this, 'handler', {
+            entry: 'backend/ticket/main.ts',
+            handler: 'lambdaHandler',
+
+        })
+        const integration = new gw.LambdaIntegration(handler);
+
+        const api = new gw.RestApi(this, 'api', {
+            restApiName: 'octankTravel'
+        });
+        const tickets = api.root.addResource('tickets', {
+            defaultIntegration: integration,
+            defaultCorsPreflightOptions: {
+                allowOrigins: gw.Cors.ALL_ORIGINS,
+                allowMethods: gw.Cors.ALL_METHODS,
+            }
+        });
+
+        // new gw.LambdaRestApi(this, 'gw', {
+        //     handler: handler,
+        // })
+    }
+}
