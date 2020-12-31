d4db074fd225f22e1ef7a0c336c3ba89334db703
tmokmss <tomookam@live.jp>
2020-12-31 16:33:17 +0900

    rename stage to dev

--- lib/stack/cicd-backend.ts
+++ lib/stack/cicd-backend.ts
@@ -2,35 +2,34 @@ import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
 import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
-import { TravelStage } from '../stage/travel';
-import { TicketServiceStack } from './ticket-service';
+import { DevStage } from '../stage/dev';
 
 export class CicdBackendStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
-      super(scope, id, props);
-  
-      const sourceArtifact = new codepipeline.Artifact();
-      const cloudAssemblyArtifact = new codepipeline.Artifact();
-   
-      const pipeline = new CdkPipeline(this, 'Pipeline', {
-        pipelineName: 'OctankTravelBackend',
-        cloudAssemblyArtifact,
-  
-        sourceAction: new codepipeline_actions.GitHubSourceAction({
-          actionName: 'GitHub',
-          output: sourceArtifact,
-          oauthToken: SecretValue.secretsManager('github-token'),
-          owner: 'tmokmss',
-          repo: 'ticket-backend',
-        }),
-  
-         synthAction: SimpleSynthAction.standardNpmSynth({
-           sourceArtifact,
-           cloudAssemblyArtifact,
-           buildCommand: 'npm run build'
-         }),
-      });
+        super(scope, id, props);
 
-      pipeline.addApplicationStage(new TravelStage(this, `travel-stage`))
+        const sourceArtifact = new codepipeline.Artifact();
+        const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+        const pipeline = new CdkPipeline(this, 'Pipeline', {
+            pipelineName: 'OctankTravelBackend',
+            cloudAssemblyArtifact,
+
+            sourceAction: new codepipeline_actions.GitHubSourceAction({
+                actionName: 'GitHub',
+                output: sourceArtifact,
+                oauthToken: SecretValue.secretsManager('github-token'),
+                owner: 'tmokmss',
+                repo: 'ticket-backend',
+            }),
+
+            synthAction: SimpleSynthAction.standardNpmSynth({
+                sourceArtifact,
+                cloudAssemblyArtifact,
+                buildCommand: 'npm run build'
+            }),
+        });
+
+        pipeline.addApplicationStage(new DevStage(this, `dev-stage`))
     }
-  }
+}
--- lib/stage/dev.ts
+++ lib/stage/dev.ts
@@ -2,7 +2,7 @@ import { Construct, Stage, StageProps } from "@aws-cdk/core";
 import { StorageStack } from "../stack/storage";
 import { TicketServiceStack } from "../stack/ticket-service";
 
-export class TravelStage extends Stage {
+export class DevStage extends Stage {
     constructor(scope: Construct, id: string, props?: StageProps) {
         super(scope, id, props);
 
