c6b736611b9bc629f1669d8e397320334d9fa63a
tmokmss <tomookam@live.jp>
2021-01-06 10:02:02 +0900

    add all stacks to cdk.ts

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
 import { CicdBackendStack } from '../lib/stack/cicd-backend';
+import { CognitoStack } from '../lib/stack/cognito';
+import { StorageStack } from '../lib/stack/storage';
+import { TicketServiceStack } from '../lib/stack/ticket-service';
 
 const prefix = 'Octank'
 const app = new cdk.App();
 
 new CicdBackendStack(app, prefix + 'CicdBackendStack');
+
+const cognito = new CognitoStack(app, 'CognitoStack');
+const ticket = new TicketServiceStack(app, 'TicketServiceStack', {
+    cognito,
+});
+new StorageStack(app, 'StorageStack');
