da0428b460f6e0b715f4209accc8b4c675696e79
tmokmss <tomookam@live.jp>
2021-01-07 11:20:01 +0900

    add dependency

--- lib/stack/ticket-service.ts
+++ lib/stack/ticket-service.ts
@@ -38,6 +38,7 @@ export class TicketServiceStack extends cdk.Stack {
                     nodeModules: [
                         "aws-serverless-express",
                         "express",
+                        "jsonwebtoken",
                     ],
                 },
                 environment: {
