fe44e3bba8bff8c4978bcf9f82007857b8af2c8a
tmokmss <tomookam@live.jp>
2021-01-22 16:48:13 +0900

    Revert "Update travel_controller.ts"
    This reverts commit 5f3ee1b3d539b0304107d689d0126bda07f685b3.

--- backend/travel/controller/travel_controller.ts
+++ backend/travel/controller/travel_controller.ts
@@ -3,16 +3,16 @@ import { Travel } from "../model/travel";
 
 export async function getTravels(req: Request, res: Response) {
     const travels = [
-        new Travel({departure: '羽田2', destination: '那覇'}),
-        new Travel({departure: '羽田2', destination: '福岡'}),
-        new Travel({departure: '羽田2', destination: '広島'}),
-        new Travel({departure: '羽田2', destination: '大阪'}),
-        new Travel({departure: '羽田2', destination: '仙台'}),
-        new Travel({departure: '成田2', destination: '那覇'}),
-        new Travel({departure: '成田2', destination: '福岡'}),
-        new Travel({departure: '成田2', destination: '広島'}),
-        new Travel({departure: '成田2', destination: '大阪'}),
-        new Travel({departure: '成田2', destination: '仙台'}),
+        new Travel({departure: '羽田', destination: '那覇'}),
+        new Travel({departure: '羽田', destination: '福岡'}),
+        new Travel({departure: '羽田', destination: '広島'}),
+        new Travel({departure: '羽田', destination: '大阪'}),
+        new Travel({departure: '羽田', destination: '仙台'}),
+        new Travel({departure: '成田', destination: '那覇'}),
+        new Travel({departure: '成田', destination: '福岡'}),
+        new Travel({departure: '成田', destination: '広島'}),
+        new Travel({departure: '成田', destination: '大阪'}),
+        new Travel({departure: '成田', destination: '仙台'}),
     ];
 
     res.type('json');
