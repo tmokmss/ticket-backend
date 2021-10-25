c1c370d4c7f3a2c45119bac2c848b91e8334117e
tmokmss <tomookam@live.jp>
2021-10-25 15:51:23 +0900

    Update cicd-backend.ts

--- lib/stack/cicd-backend.ts
+++ lib/stack/cicd-backend.ts
@@ -1,80 +1,84 @@
-import * as codepipeline from '@aws-cdk/aws-codepipeline';
-import * as sns from '@aws-cdk/aws-sns';
-import * as sub from '@aws-cdk/aws-sns-subscriptions';
-import * as lambdanode from '@aws-cdk/aws-lambda-nodejs';
-import * as codebuild from '@aws-cdk/aws-codebuild';
-import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
-import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
-import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
-import { DevStage } from '../stage/dev';
-import { CodePipelineNotification } from '../construct/codepipeline-notification';
+import * as sns from "@aws-cdk/aws-sns";
+import * as sub from "@aws-cdk/aws-sns-subscriptions";
+import * as lambdanode from "@aws-cdk/aws-lambda-nodejs";
+import * as codebuild from "@aws-cdk/aws-codebuild";
+import { Construct, SecretValue, Stack, StackProps } from "@aws-cdk/core";
+import { DevStage } from "../stage/dev";
+import { CodePipelineNotification } from "../construct/codepipeline-notification";
+import * as pipelines from "@aws-cdk/pipelines";
 
 export class CicdBackendStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        const sourceArtifact = new codepipeline.Artifact();
-        const cloudAssemblyArtifact = new codepipeline.Artifact();
+    const source = pipelines.CodePipelineSource.gitHub(
+      `tmokmss/ticket-backend`,
+      "main",
+      {
+        authentication: SecretValue.secretsManager("github-token"),
+      }
+    );
 
-        const pipeline = new CdkPipeline(this, 'Pipeline', {
-            pipelineName: 'OctankTravelBackend',
-            cloudAssemblyArtifact,
+    const pipeline = new pipelines.CodePipeline(this, "Pipeline", {
+      pipelineName: "OctankTravelBackend",
+      selfMutation: false,
+      synth: new pipelines.ShellStep("Synth", {
+        input: source,
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
 
-            sourceAction: new codepipeline_actions.GitHubSourceAction({
-                actionName: 'GitHub',
-                output: sourceArtifact,
-                oauthToken: SecretValue.secretsManager('github-token'),
-                owner: 'tmokmss',
-                repo: 'ticket-backend',
-                branch: 'main',
-            }),
+    const testStep = new pipelines.CodeBuildStep(`TestStep`, {
+    //   partialBuildSpec: codebuild.BuildSpec.fromSourceFilename(
+    //     "backend/ticket/buildspec.yml"
+    //   ),
+      buildEnvironment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_4_0,
+        computeType: codebuild.ComputeType.SMALL,
+        privileged: true,
+      },
+      commands: [
+        "npm run test",
+      ],
+      installCommands: [
+        "docker run -d -p 8000:8000 public.ecr.aws/mobileup/dynamodb-local",
+        "cd backend/ticket",
+        "npm ci",
+        "npm run build",
+      ],
+      input: source,
+    });
 
-            synthAction: SimpleSynthAction.standardNpmSynth({
-                sourceArtifact,
-                cloudAssemblyArtifact,
-                buildCommand: 'npm run build',
-            }),
-        });
+    pipeline.addStage(new DevStage(this, `dev-stage`), {
+        pre: [testStep],
+    });
 
-        const testStage = pipeline.addStage('Test');
-        testStage.addActions(
-            new codepipeline_actions.CodeBuildAction({
-                actionName: 'Test-TicketService',
-                input: sourceArtifact,
-                project: new codebuild.PipelineProject(this, 'ticket-service-test-project', {
-                    environment: {
-                        buildImage: codebuild.LinuxBuildImage.STANDARD_4_0,
-                        computeType: codebuild.ComputeType.SMALL,
-                        privileged: true,
-                    },
-                    buildSpec: codebuild.BuildSpec.fromSourceFilename('backend/ticket/buildspec.yml'),
-                }),
-            }),
-        );
+    pipeline.buildPipeline();
 
-        pipeline.addApplicationStage(new DevStage(this, `dev-stage`));
+    const topic = new sns.Topic(this, `topic`);
+    new CodePipelineNotification(this, `notification`, {
+      topic: topic,
+      pipeline: pipeline.pipeline,
+    });
 
-        const topic = new sns.Topic(this, `topic`);
-        new CodePipelineNotification(this, `notification`, {
-            topic: topic,
-            pipeline: pipeline.codePipeline,
-        });
+    const slackNotifier = new lambdanode.NodejsFunction(
+      this,
+      "slack-notifier",
+      {
+        entry: "backend/slack-notify/main.ts",
+        handler: "lambdaHandler",
+        depsLockFilePath: "backend/slack-notify/package-lock.json",
+        bundling: {
+          minify: false,
+          nodeModules: ["axios"],
+        },
+        environment: {
+          SLACK_WEBHOOK_URL:
+            SecretValue.secretsManager("slack-webhook-url").toString(),
+        },
+      }
+    );
 
-        const slackNotifier = new lambdanode.NodejsFunction(this, 'slack-notifier', {
-            entry: 'backend/slack-notify/main.ts',
-            handler: 'lambdaHandler',
-            depsLockFilePath: 'backend/slack-notify/package-lock.json',
-            bundling: {
-                minify: false,
-                nodeModules: [
-                    "axios",
-                ],
-            },
-            environment: {
-                "SLACK_WEBHOOK_URL": SecretValue.secretsManager('slack-webhook-url').toString(),
-            }
-        });
-
-        topic.addSubscription(new sub.LambdaSubscription(slackNotifier));
-    }
+    topic.addSubscription(new sub.LambdaSubscription(slackNotifier));
+  }
 }
